b0e00e494334e8dff1516893f11112fcfa5561eb
Amit-exe <amit.kushwaha.exe@gmail.com>
2025-01-12 23:56:23 +0530

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,74 +1,65 @@
-import './App.css';
-import Footer from './components/Footer'
-import Header from './components/Header'
-import FoodItem from './components/FoodItem';
-import mainImg from './images/bg1.jpg'
-import menu from "./menuItems"
-import drinks from "./drinks"
-import events from "./events"
-import About from './components/about';
-import Event from './components/Events';
-
-
-
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import FoodItem from "./components/FoodItem";
+import mainImg from "./images/bg1.jpg";
+import menu from "./menuItems";
+import drinks from "./drinks";
+import events from "./events";
+import About from "./components/about";
+import Event from "./components/Events";
 
 function App() {
   return (
     <div>
-
       <section id="home">
-      <Header />
+        <Header />
       </section>
 
       <section id="menu">
-      <h1>Menu items</h1>
-      {menu.map((item)=> (<FoodItem
-      img={item.img}
-      foodname={item.name}
-      rating={item.rating}
-      price={item.price} />))}
+        <h1>Menu items</h1>
+        {menu.map((item, i) => (
+          <FoodItem
+            key={i}
+            img={item.img}
+            foodname={item.name}
+            rating={item.rating}
+            price={item.price}
+          />
+        ))}
       </section>
 
       <section id="drinks">
         <h1>Drinks</h1>
-        {drinks.map((item)=> (<FoodItem
-        img={item.img}
-        foodname={item.name}
-        rating={item.rating}
-        price={item.price} />))}
-
+        {drinks.map((item, i) => (
+          <FoodItem
+            img={item.img}
+            key={i}
+            foodname={item.name}
+            rating={item.rating}
+            price={item.price}
+          />
+        ))}
       </section>
 
       <section id="events">
         <h1>Events</h1>
-        {events.map(event => (<Event 
-          name={event.name}
-          start={event.start}
-          end={event.end}
-          date={event.date}
-          offer={event.offers}
-        />))}
-        
-        
+        {events.map((event, i) => (
+          <Event
+            name={event.name}
+            key={i}
+            start={event.start}
+            end={event.end}
+            date={event.date}
+            offer={event.offers}
+          />
+        ))}
       </section>
-
-      
 
       <section id="about">
-        <About/>
-        <Footer/>
-
+        <About />
+        <Footer />
       </section>
-
-
-
-
-
-
-
-
-
-
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,93 @@
-import React from "react";
-import logo from "../images/logo.png"
+import React, { useEffect } from "react";
+import logo from "../images/logo.png";
 
 function Navbar() {
-    return <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="">
-          <img className="logo" src={logo}></img>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+  // const navlinks = document.querySelector(".navbar-nav");
+  // navlinks.addEventListener("click", function (e) {
+  //   e.preventDefault();
+  //   const secid = e.target.getAttribute("href");
 
+  //   const sec = document.querySelector(secid);
+  //   sec.scrollIntoView({ behavior: "smooth" });
+  //   console.log(sec);
+  // });
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#menu">Menu</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#drinks">Drinks</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#events">Events</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
-            </li>
-          </ul>
-        </div>
-      </nav>;
+  useEffect(() => {
+    const navlinks = document.querySelector(".navbar-nav");
+
+    if (navlinks) {
+      navlinks.addEventListener("click", function (e) {
+        if (e.target.tagName === "A") {
+          e.preventDefault();
+          const secid = e.target.getAttribute("href");
+
+          if (secid) {
+            const sec = document.querySelector(secid);
+            if (sec) {
+              sec.scrollIntoView({ behavior: "smooth" });
+              console.log(sec);
+            }
+          }
+        }
+      });
+    }
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      if (navlinks) {
+        navlinks.removeEventListener("click", () => {});
+      }
+    };
+  }, []);
+
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="">
+        <img className="logo" src={logo}></img>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#home">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#menu">
+              Menu
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#drinks">
+              Drinks
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#events">
+              Events
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
